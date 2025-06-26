8324f80800a1638192af130c0ca3a9fd5d8952bf
Dahyeon Jin <wlsekgus12@gmail.com>
2025-06-27 02:18:51 +0900

    [Lv.1 푸드 파이트 대회] 문제 설명 및 풀이 추가

---
+++ tests/week2/dahyeon-test-solution.js
@@ -0,0 +1,18 @@
+function solution(food) {
+  let foodPlacement = '0'; // 가운데 물 1개
+
+  for (let i = food.length - 1; i > 0; i--) {
+    let currFood = food[i];
+
+    // 홀수면 -1 해서 짝수로 만듦
+    if (currFood % 2 === 1) currFood -= 1;
+
+    // 음식 절반을 왼쪽과 오른쪽 양끝에 배치
+    const halfFood = currFood / 2;
+    // repeat(): 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열 반환
+    const foodStr = String(i).repeat(halfFood);
+    foodPlacement = foodStr + foodPlacement + foodStr;
+  }
+
+  return foodPlacement;
+}
